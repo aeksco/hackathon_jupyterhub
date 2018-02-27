82e4aacff90660438ee6a13e6ad28f2044f5f478
Alexander Schwartzberg <aeksco@gmail.com>
2018-02-27 14:38:59 -0500

    Added internals/install_python2_kernel.sh

--- jupyterhub/internals/install_python2_kernel.sh
+++ jupyterhub/internals/install_python2_kernel.sh
@@ -1,8 +1,8 @@
 echo "INSTALLING PYTHON 2.7 KERNEL"
 
 # Creates Python 2.7 Conda environment
-conda create --name py27 python=2.7
-conda install -n py27 anaconda
+conda create --name py27 python=2.7 -y
+conda install -n py27 anaconda -y
 
 # Activate Python 2.7 environment
 source activate py27
@@ -11,10 +11,10 @@ source activate py27
 pip install bash_kernel
 python -m bash_kernel.install
 
-# TODO - install Python 2.7 dependencies...
-
 # Install Python 2.7 Jupyter kernel
 ipython kernel install
+
+# TODO - install Python 2.7 libraries here
 
 # Deactivates Python 2.7 environment (jumps back to Python 3.5.5)
 source deactivate
