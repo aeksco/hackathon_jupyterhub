1f25d63ee1aef0423147dd06bc848d58aa3c4602
Alexander Schwartzberg <aeksco@gmail.com>
2018-02-27 14:24:54 -0500

    Moved Python 2.7 kernel install into internals shell script

--- jupyterhub/internals/test.sh
+++ jupyterhub/internals/test.sh
@@ -1,1 +1,20 @@
-echo "HELLO FROM INTERNALS!"
+echo "INSTALLING PYTHON 2.7 KERNEL"
+
+# Creates Python 2.7 Conda environment
+conda create --name py27 python=2.7
+conda install -n py27 anaconda
+
+# Activate Python 2.7 environment
+source activate py27
+
+# Install bash kernel
+pip install bash_kernel
+python -m bash_kernel.install
+
+# TODO - install Python 2.7 dependencies...
+
+# Install Python 2.7 Jupyter kernel
+ipython kernel install
+
+# Deactivates Python 2.7 environment (jumps back to Python 3.5.5)
+source deactivate
