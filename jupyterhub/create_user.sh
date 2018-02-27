10608a1beb4a3daec6fb18711bf44904acab23cf
Alexander Schwartzberg <aeksco@gmail.com>
2018-02-27 13:15:00 -0500

    provision.sh & create_user.sh scripts

--- jupyterhub/create_user.sh
+++ jupyterhub/create_user.sh
@@ -1,6 +1,27 @@
-# TODO - this script should accept a single username argument..
-# while read p; do
-#   adduser --disabled-password --gecos "" $p
-#   mkdir /home/$p/notebooks
-#   chown -R $p /home/$p/
-# done <users.txt
+# Prompt
+echo \n\nCREATING USER: $1\n\n
+
+# adduser - creates a new Unix user account
+adduser --disabled-password --gecos "" $1
+
+# Creates ~/notebooks
+mkdir /home/$1/notebooks
+
+# Creates ~/notebooks/WELCOME.md
+# TODO - this file needs _actual_ contents
+touch /home/$1/notebooks/WELCOME.md
+echo HELLO > /home/$1/notebooks/WELCOME.md
+
+# Creates ~/notebooks/share
+# This will be a sorta airdrop for the hackathon, maybe..
+mkdir /home/$1/notebooks/share
+
+# Creates ~/notebooks/share/INSTRUCTIONS.md
+# TODO - this file needs contents
+touch /home/$1/notebooks/share/INSTRUCTIONS.md
+echo HELLO > /home/$1/notebooks/share/INSTRUCTIONS.md
+
+# Recursively sets the owner of ~/
+chown -R $1 /home/$1/
+
+echo \n\nDONE CREATING USER: $1\n\n
--- jupyterhub/provision.sh
+++ jupyterhub/provision.sh
@@ -1,30 +1,6 @@
-# TODO - run sh create_user.sh $p
-# Instead of script above..
+# Creates a new user account for each user in users.txt
 while read p; do
-
-  # adduser - creates a new Unix user account
-  adduser --disabled-password --gecos "" $p
-
-  # Creates ~/notebooks
-  mkdir /home/$p/notebooks
-
-  # Creates ~/notebooks/WELCOME.md
-  # TODO - this file needs _actual_ contents
-  touch /home/$p/notebooks/WELCOME.md
-  echo HELLO > /home/$p/notebooks/WELCOME.md
-
-  # Creates ~/notebooks/share
-  # This will be a sorta airdrop for the hackathon, maybe..
-  mkdir /home/$p/notebooks/share
-
-  # Creates ~/notebooks/share/INSTRUCTIONS.md
-  # TODO - this file needs contents
-  touch /home/$p/notebooks/share/INSTRUCTIONS.md
-  echo HELLO > /home/$p/notebooks/share/INSTRUCTIONS.md
-
-  # Recursively sets the owner of ~/
-  chown -R $p /home/$p/
-
+  sh create_user.sh $p
 done <users.txt
 
 # Creates shared directories
