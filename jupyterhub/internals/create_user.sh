a37b4b6196f5f4288296e9f287707cce1adbfc29
Alexander Schwartzberg <aeksco@gmail.com>
2018-02-28 18:23:00 -0500

    create_user.sh - copies WELCOME.md from docs

--- jupyterhub/internals/create_user.sh
+++ jupyterhub/internals/create_user.sh
@@ -9,17 +9,18 @@ mkdir /home/$1/notebooks
 
 # Creates ~/notebooks/WELCOME.md
 # TODO - this file needs _actual_ contents
-touch /home/$1/notebooks/WELCOME.md
-echo HELLO > /home/$1/notebooks/WELCOME.md
+# touch /home/$1/notebooks/WELCOME.md
+# echo HELLO > /home/$1/notebooks/WELCOME.md
+cp ./docs/WELCOME.md /home/$1/notebooks
 
 # Creates ~/notebooks/share
 # This will be a sorta airdrop for the hackathon, maybe..
-mkdir /home/$1/notebooks/share
+# mkdir /home/$1/notebooks/share
 
 # Creates ~/notebooks/share/INSTRUCTIONS.md
 # TODO - this file needs contents
-touch /home/$1/notebooks/share/INSTRUCTIONS.md
-echo HELLO > /home/$1/notebooks/share/INSTRUCTIONS.md
+# touch /home/$1/notebooks/share/INSTRUCTIONS.md
+# echo HELLO > /home/$1/notebooks/share/INSTRUCTIONS.md
 
 # Recursively sets the owner of ~/
 chown -R $1 /home/$1/
