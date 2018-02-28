77802fd4513c2f85bc2886b85df7462ad8f2713e
Alexander Schwartzberg <aeksco@gmail.com>
2018-02-28 18:23:16 -0500

    Provision installs R libraries

--- jupyterhub/internals/install_r_kernel.sh
+++ jupyterhub/internals/install_r_kernel.sh
@@ -1,2 +1,28 @@
 echo "INSTALLING R MARKDOWN KERNEL"
 conda install -c r r-essentials -y
+
+# R packages including IRKernel which gets installed globally.
+conda config --system --append channels r
+
+# R Package installations...
+conda install --quiet --yes \
+'rpy2=2.8*' \
+'r-base=3.3.2' \
+'r-irkernel=0.7*' \
+'r-plyr=1.8*' \
+'r-devtools=1.12*' \
+'r-tidyverse=1.0*' \
+'r-shiny=0.14*' \
+'r-rmarkdown=1.2*' \
+'r-forecast=7.3*' \
+'r-rsqlite=1.1*' \
+'r-reshape2=1.4*' \
+'r-nycflights13=0.2*' \
+'r-caret=6.0*' \
+'r-rcurl=1.95*' \
+'r-crayon=1.3*' \
+'r-randomforest=4.6*'
+
+# Conda clean & permissions fix
+conda clean -tipsy
+fix-permissions $CONDA_DIR
--- jupyterhub/internals/provision.sh
+++ jupyterhub/internals/provision.sh
@@ -3,13 +3,6 @@ while read p; do
   sh create_user.sh $p
 done <users.txt
 
-# Creates shared directories
-# mkdir /home/share
-# mkdir /home/share/datasets
-# mkdir /home/share/training_sets
-# mkdir /home/share/examples
-# touch /home/share/share.md
-
 # Update / Install libraries
 apt-get update
 apt-get install unzip git nano
@@ -19,13 +12,5 @@ sh install_python2_libs.sh
 sh install_python3_libs.sh
 sh install_r_kernel.sh
 
-# Google Deep Dream example
-# Fetch BVLC GoogleNet Caffe Model
-# https://github.com/google/deepdream/blob/master/dream.ipynb
-# https://github.com/BVLC/caffe/tree/master/models/bvlc_googlenet
-# wget http://dl.caffe.berkeleyvision.org/bvlc_googlenet.caffemodel
-# wget https://raw.githubusercontent.com/BVLC/caffe/master/models/bvlc_googlenet/deploy.prototxt
-
-# Another Deep Dream example...
-# https://nbviewer.jupyter.org/github/tensorflow/tensorflow/blob/master/tensorflow/examples/tutorials/deepdream/deepdream.ipynb
-# https://photos.google.com/share/AF1QipPX0SCl7OzWilt9LnuQliattX4OUCj_8EP65_cTVnBmS1jnYgsGQAieQUc1VQWdgQ?key=aVBxWjhwSzg2RjJWLWRuVFBBZEN1d205bUdEMnhB
+# Builds share & examples directories
+sh build_examples.sh
