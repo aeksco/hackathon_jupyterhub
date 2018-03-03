e7ddc231abd63f2879efafff5fbe63e850521de6
Alexander Schwartzberg <aeksco@gmail.com>
2018-03-03 09:55:54 -0500

    Added apt-get install curl to provision.sh

--- jupyterhub/internals/provision.sh
+++ jupyterhub/internals/provision.sh
@@ -5,7 +5,7 @@ done <users.txt
 
 # Update / Install libraries
 apt-get update
-apt-get -Y install unzip zip git nano sudo
+apt-get -Y install unzip zip git nano sudo curl
 
 # Install Python & R kernels and libraries
 sh install_python2_libs.sh
