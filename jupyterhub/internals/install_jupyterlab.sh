b308f7af30ebdf994fd2fbbd145eed6ffaf8b90e
Alexander Schwartzberg <aeksco@gmail.com>
2018-03-25 15:33:08 -0400

    Added install_jupyterlab.sh

---
+++ jupyterhub/internals/install_jupyterlab.sh
@@ -0,0 +1,7 @@
+
+# Installs Jupyterlab (optional)
+conda install --yes jupyterlab
+
+# Install Jupyterlab Hub extension
+# https://github.com/jupyterhub/jupyterlab-hub
+jupyter labextension install @jupyterlab/hub-extension
